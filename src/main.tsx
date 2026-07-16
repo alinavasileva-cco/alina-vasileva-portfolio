5921d2ecea1508f108a4724d6b57d17c8f12d465
Commercial <135322806+alinavasileva-cco@users.noreply.github.com>
2026-07-16 21:47:01 +0300

    Load responsive and typography QA styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "./responsive-fixes.css";
+import "./typography-fixes.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
