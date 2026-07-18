dc7cb3cf4a84e5e1aa8d7638f95c7638a8aac157
Commercial <135322806+alinavasileva-cco@users.noreply.github.com>
2026-07-18 21:58:58 +0300

    Publish premium editorial portfolio design

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./brutal-site.css";
+import "./luxury-site.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
